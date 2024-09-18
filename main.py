024982014e9ccd969a73854c7f0c0acbeeae311d
vSoma0 <wessambinhussam@gmail.com>
2024-09-18 12:12:19 +0300

    vercel deployment

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from security import get_api_key
 from models import QueryRequest
 from vector import search_vectorstore 
 from dotenv import load_dotenv
-load_dotenv()
+from fastapi.middleware.cors import CORSMiddleware
 
+load_dotenv()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/query")
 async def search_query(query_request: QueryRequest, api_key: str = Depends(get_api_key)):
